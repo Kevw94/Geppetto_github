ee8ece65863beda405e00b259b47a033dc2174c1
Kevw94 <pinerokevin@icloud.com>
2025-11-20 15:26:54 +0100

    Implement collecte balles avec spawn chargeur plus quand plus de balle plus de chargeur spawn

--- Assets/Scripts/MENU/AMMO/MagazineSpawner.cs
+++ Assets/Scripts/MENU/AMMO/MagazineSpawner.cs
@@ -1,11 +1,12 @@
 using UnityEngine;
-
 using System.Collections;
+using MikeNspired.XRIStarterKit;
 
 public class MagazineSpawner : MonoBehaviour
 {
     public GameObject magazinePrefab;
     public float spawnDelay = 0.5f;
+    public int magazineSize = 12;
 
     private UnityEngine.XR.Interaction.Toolkit.Interactors.XRSocketInteractor socketInteractor;
     private bool isSpawning = false;
@@ -13,7 +14,7 @@ public class MagazineSpawner : MonoBehaviour
     private void Start()
     {
         socketInteractor = GetComponent<UnityEngine.XR.Interaction.Toolkit.Interactors.XRSocketInteractor>();
-        SpawnMagazine();
+        TrySpawnMagazine();
     }
 
     private void Update()
@@ -28,11 +29,28 @@ public class MagazineSpawner : MonoBehaviour
     {
         isSpawning = true;
         yield return new WaitForSeconds(spawnDelay);
-        SpawnMagazine();
+        TrySpawnMagazine();
         isSpawning = false;
     }
 
-    private void SpawnMagazine()
+	private void TrySpawnMagazine()
+	{
+		var ammoManager = AmmoManagerLocator.Instance;
+		int currentAmmo = ammoManager?.GetAmmo() ?? -1;
+		Debug.Log($"[Spawner] Ammo Available: {currentAmmo}");
+
+		if (ammoManager != null && currentAmmo > 0 && !socketInteractor.hasSelection)
+		{
+			SpawnMagazine();
+		}
+		else
+		{
+			Debug.Log("[Spawner] Not enough ammo to spawn new magazine.");
+		}
+	}
+
+
+	private void SpawnMagazine()
     {
         GameObject newMag = Instantiate(magazinePrefab, transform.position, transform.rotation);
     }
